f24d3803ecd0fc9a7e2445b0ffd7501fdf7852bc
AgueroZZ <49107284+AgueroZZ@users.noreply.github.com>
2021-06-13 21:26:17 -0400

    updated_revision_smoothing_section

---
+++ tmb_codes/simulation_smoothing/v6/plot_for_boxplot.R
@@ -0,0 +1,25 @@
+### load packages:
+library(tidyverse)
+TEXT_SIZE = 25
+
+### load results:
+load("/Users/ziangzhang/Documents/GitHub/Paper-for-Cox-PH-Model/tmb_codes/simulation_smoothing/v6/result_300_aggregations_complicated.rda")
+load("/Users/ziangzhang/Documents/GitHub/Paper-for-Cox-PH-Model/tmb_codes/simulation_smoothing/v6/result_300_aggregations_stepwise.rda")
+load("/Users/ziangzhang/Documents/GitHub/Paper-for-Cox-PH-Model/tmb_codes/simulation_smoothing/v6/result_300_aggregations_simple.rda")
+
+### First plot: when true function is simple:
+simple_result <- result3 %>% filter(method != "MGCV") %>% select(method,coverage,mse)
+simple_result$method[simple_result$method == "AGHQ"] <- "Proposed"
+simple_result %>% ggplot(aes(x = method, y = mse)) + geom_boxplot() + theme_classic(base_size = TEXT_SIZE)
+
+
+### Second plot: when true function is stepwise:
+stepwise_result <- result2 %>% filter(method != "MGCV") %>% select(method,coverage,mse)
+stepwise_result$method[stepwise_result$method == "AGHQ"] <- "Proposed"
+stepwise_result %>% ggplot(aes(x = method, y = mse)) + geom_boxplot() + theme_classic(base_size = TEXT_SIZE)
+
+
+### Third plot: when true function is stepwise:
+com_result <- result %>% filter(method != "MGCV") %>% select(method,coverage,mse)
+com_result$method[com_result$method == "AGHQ"] <- "Proposed"
+com_result %>% ggplot(aes(x = method, y = mse)) + geom_boxplot() + theme_classic(base_size = TEXT_SIZE)
